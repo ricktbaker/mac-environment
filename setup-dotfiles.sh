08a99ffeca54165824a98e1ffcafbdf9eff05bc4
Rick Baker <>
2023-06-14 14:43:31 -0500

    dotfile setup

--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-ln -s ~/.p10k.zsh /Work/general/mac-environment/dotfiles/p10k.zsh
-ln -s ~/.oh-my-zsh-custom /Work/general/mac-environment/dotfiles/oh-my-zsh-custom
-ln -s ~/.zshrc /Work/general/mac-environment/dotfiles/zshrc
+ln -s ~/.p10k.zsh ~/Work/general/mac-environment/dotfiles/p10k.zsh
+ln -s ~/.oh-my-zsh-custom ~/Work/general/mac-environment/dotfiles/oh-my-zsh-custom
+ln -s ~/.zshrc ~/Work/general/mac-environment/dotfiles/zshrc
