60f91395f1aecada1032f6181216da6acfa95c4c
Rick Baker <>
2023-06-16 12:31:00 -0500

    fix elseif

--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -7,14 +7,15 @@ ln -s ~/.oh-my-zsh-custom ~/Work/general/mac-environment/dotfiles/global/oh-my-z
 ln -s ~/.zshrc ~/Work/general/mac-environment/dotfiles/global/zshrc
 
 COMPUTER_NAME=$(scutil --get ComputerName)
-if [[ $COMPUTER_NAME -eq "forta" ]]; then
+if [[ $COMPUTER_NAME eq "forta" ]]; then
   echo "Setting up Forta environment"
-elif [[ $COMPUTER_NAME -eq "LOXC02G80QPMD6Q"]]; then
+elif [[ $COMPUTER_NAME eq "LOXC02G80QPMD6Q" ]]; then
   echo "Setting up Assurant environment"
 elif [[ $COMPUTER_NAME eq "syneos" ]]; then
   echo "Setting up Syneos environment"
 fi
 
+# Install asdf versions
 cd ~/ && asdf install
 
 
